1d16cc8c8977dbe2d710ea427908c2204074d8c6
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2024-06-19 09:54:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+function calc() {
+    let number1 = parseFloat(document.getElementById("firstNumber").value);
+    let number2 = parseFloat(document.getElementById("secondNumber").value);
+    let result = number1 + number2;
+    
+}
+
+
