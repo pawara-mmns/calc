7321cee2611209eed5246f14aabd5e366f678446
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2024-07-03 11:25:43 +0530

    "Updated app.js to parse country data and generate HTML table, and added table element to index.html"

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 fetch("https://restcountries.com/v3.1/all")
 .then(res=>res.json())
-.then(data=>console.log(data))
+.then(data=>{
+    let tblCountries=document.getElementById("tbl");
+
+    let tblBody=`<tr>
+                    <th>Name</th>
+                    <th>Flag</th>
+                </tr>`;
+
+    data.forEach(element => {
+        tblBody+=`<tr>
+                    <td>${element.name.common}</td>
+                    <td>${element.flag}</td>
+                </tr>`
+
+        console.log(element.name.common);
+    });
+
+    tblCountries.innerHTML=tblBody;
+})
