28d6be7d0cd0e8e5e8c9b448568de0c224ec75c5
Pawara Samarawickrama <pawarasamarawickrama@gmail.com>
2024-07-03 14:19:12 +0530

    Added country details display, search functionality, and styling to the country information app.

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 fetch("https://restcountries.com/v3.1/all")
-.then(res=>res.json())
-.then(data=>{
-    let tblCountries=document.getElementById("tbl");
+    .then(res => res.json())
+    .then(data => {
+        data.sort((a, b) => a.name.common.localeCompare(b.name.common));
+        
+        let tblCountries = document.getElementById("tbl");
 
-    let tblBody=`<tr>
-                    <th>Name</th>
-                    <th>Flag</th>
-                </tr>`;
+        let tblBody = `<tr>
+                        <th>Name</th>
+                        <th>Flag</th>
+                    </tr>`;
 
-    data.forEach(element => {
-        tblBody+=`<tr>
-                    <td>${element.name.common}</td>
-                    <td>${element.flag}</td>
-                </tr>`
+        data.forEach(element => {
+            tblBody += `<tr onclick="displayCountryDetails('${element.name.common}')">
+                        <td>${element.name.common}</td>
+                        <td><img src="${element.flags.png}" alt="Flag of ${element.name.common}" width="30"></td>
+                    </tr>`;
+        });
 
-        console.log(element.name.common);
+        tblCountries.innerHTML = tblBody;
     });
 
-    tblCountries.innerHTML=tblBody;
-})
+function displayCountryDetails(countryName) {
+    fetch(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(res => res.json())
+        .then(data => {
+            let country = data[0];
+            
+            document.getElementById("offitalName").innerText = `Official Name: ${country.name.official}`;
+            document.getElementById("name").innerText = `Common Name: ${country.name.common}`;
+            document.getElementById("img").innerHTML = `<img src="${country.flags.png}" alt="Flag of ${country.name.common}" width="100">`;
+            document.getElementById("population").innerText = `Population: ${country.population.toLocaleString()}`;
+            document.getElementById("region").innerText = `Region: ${country.region}`;
+            document.getElementById("subregion").innerText = `Subregion: ${country.subregion}`;
+            document.getElementById("capital").innerText = `Capital: ${country.capital}`;
+            document.getElementById("mapsLink").href = country.maps.googleMaps;
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function serchCuntrie() {
+    let searchValue = document.getElementById("txtSearchValue").value;
+    displayCountryDetails(searchValue);
+}
